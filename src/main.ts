5156fb445a5ee452b726df39bc9f6fc5a5481795
Brandon Farmer <bthesorceror@gmail.com>
2018-04-29 17:32:12 -0700

    Fix command options

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import axios from "axios";
-import * as commander from "commander";
-import * as fs from "fs";
-import * as HtmlEntities from "html-entities";
-import { InfluxDB } from "influx";
-import * as path from "path";
-import * as Sentiment from "sentiment";
-import * as Twit from "twit";
+import axios from 'axios';
+import * as commander from 'commander';
+import * as fs from 'fs';
+import * as HtmlEntities from 'html-entities';
+import { InfluxDB } from 'influx';
+import * as path from 'path';
+import * as Sentiment from 'sentiment';
+import * as Twit from 'twit';
 
 interface IOptions {
   url: string;
@@ -45,15 +45,15 @@ interface IGeocodedLocation {
 
 const geocodeToTwitterLocation = (result: IGeocodedLocationResult): string => {
   const {
-    geometry: { bounds }
+    geometry: { bounds },
   } = result;
 
   return [
     bounds.southwest.lng,
     bounds.southwest.lat,
     bounds.northeast.lng,
-    bounds.northeast.lat
-  ].join(",");
+    bounds.northeast.lat,
+  ].join(',');
 };
 
 interface IParams extends Twit.Params {
@@ -67,11 +67,11 @@ const createTwitterChannels = (
   const client = new Twit(config);
   const params: IParams = { locations: location };
 
-  return client.stream("statuses/filter", params);
+  return client.stream('statuses/filter', params);
 };
 
 const parseConfig = (filepath: string): IConfig => {
-  const contents: string = fs.readFileSync(filepath, { encoding: "utf8" });
+  const contents: string = fs.readFileSync(filepath, { encoding: 'utf8' });
   const results: IConfig = JSON.parse(contents);
 
   return results;
@@ -84,9 +84,9 @@ const createInfluxClient = (url: string, database: string): InfluxDB => {
 const geocodeLocation = async (
   address: string
 ): Promise<IGeocodedLocationResult> => {
-  const url = "https://maps.googleapis.com/maps/api/geocode/json";
+  const url = 'https://maps.googleapis.com/maps/api/geocode/json';
   const results = await axios.get<IGeocodedLocation>(url, {
-    params: { address, sensor: false }
+    params: { address, sensor: false },
   });
 
   return results.data.results[0];
@@ -94,26 +94,27 @@ const geocodeLocation = async (
 
 const start = async () => {
   commander
-    .version("1.0.0")
+    .name('location-tweets')
+    .version('1.0.0')
     .option(
-      "-u, --url <url>",
-      "url to influx instance",
-      "http://localhost:8086"
+      '-u, --url <url>',
+      'url to influx instance',
+      'http://localhost:8086'
     )
     .option(
-      "-d, --database <database>",
-      "influxdb database to store data",
-      "tweets"
+      '-d, --database <database>',
+      'influxdb database to store data',
+      'tweets'
     )
     .option(
-      "-c, --config <path>",
-      "path to twitter config file",
-      path.join(process.env.HOME || "", ".tweets.json")
+      '-c, --config <path>',
+      'path to twitter config file',
+      path.join(process.env.HOME || '', '.tweets.json')
     )
     .option(
-      "-l, --location <city,state>",
-      "location that you want to monitor tweets from",
-      "San Francisco, CA"
+      '-l, --location <city,state>',
+      'location that you want to monitor tweets from',
+      'San Francisco, CA'
     )
     .parse(process.argv);
 
@@ -130,20 +131,20 @@ const start = async () => {
   const decoder = new HtmlEntities.AllHtmlEntities();
   const analyzer = new Sentiment();
 
-  stream.on("tweet", (tweet: Twit.Twitter.Status) => {
+  stream.on('tweet', (tweet: Twit.Twitter.Status) => {
     const text = decoder.decode(tweet.text);
     const analysis = analyzer.analyze(text);
 
     const { score, comparative } = analysis;
     const fields = { score, comparative, text, charCount: text.length };
 
-    influxClient.writeMeasurement("sentiment", [
+    influxClient.writeMeasurement('sentiment', [
       {
         fields,
         tags: {
-          location
-        }
-      }
+          location,
+        },
+      },
     ]);
   });
 };
